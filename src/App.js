d9680ab6e2df135e536634d16d1ff79560bacf66
Diwakar-Rai <rdiwakark23@gmail.com>
2022-10-09 19:08:39 +0530

    added a menu component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
+import "./App.css";
+// import { DISHES } from "./shared/dish";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante de confusion</NavbarBrand>
+        </div>
+      </Navbar>
+      {/* <Menu dishes={DISHES} /> */}
+      <Menu/>
     </div>
   );
 }
